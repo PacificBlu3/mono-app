f23c10696f533988d726df618101023592f47d74
Siddharth Basireddy <siddharth.basireddy@atos.net>
2023-12-09 21:26:19 +0100

    Login workflow

--- auth/auth.go
+++ auth/auth.go
@@ -16,11 +16,28 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Authorization", "nfn61xt4udbi793p")
 		w.WriteHeader(http.StatusOK)
 	}
+}
 
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	cookie := http.Cookie{
+		Name:     "OAuth2-Token",
+		Value:    "nfn61xt4udbi793p",
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+	}
+
+	// Use the http.SetCookie() function to send the cookie to the client.
+	// Behind the scenes this adds a `Set-Cookie` header to the response
+	// containing the necessary cookie data.
+	http.SetCookie(w, &cookie)
 }
 
 func main() {
 	http.HandleFunc("/auth", authHandler)
+	http.HandleFunc("/login", loginHandler)
 
 	err := http.ListenAndServe(":4128", nil)
 	if errors.Is(err, http.ErrServerClosed) {
